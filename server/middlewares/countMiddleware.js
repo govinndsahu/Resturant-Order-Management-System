8e6e607cf1c985b3db5127d6ea09011e7f13b7e0
GOVIND <work.govindsahu@gmail.com>
2026-07-12 14:56:04 +0530

    patch-check-count-middleware

--- server/middlewares/countMiddleware.js
+++ server/middlewares/countMiddleware.js
@@ -65,7 +65,7 @@ export const resetCount = async (req, res, next) => {
       count = newCount;
     }
 
-    if (count.maxCount > 100) {
+    if (count.maxCount <= 100) {
       req.count = count;
       next();
     }
